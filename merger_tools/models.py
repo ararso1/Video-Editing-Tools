a635b84ecf7d57948e72b02c35a96a8f814e2874
ararso <arasoalisho2@gmail.com>
2022-12-14 09:45:06 +0300

    update

--- merger_tools/models.py
+++ merger_tools/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from django.core.validators import FileExtensionValidator
+from time import timezone
 # Create your models here.
+
+class Video(models.Model):
+
+    original_video = models.FileField(upload_to='videos_uploaded',null=True,
+        validators=[FileExtensionValidator(allowed_extensions=['MOV','avi','mp4','webm','mkv'])])
+    merged_video = models.FileField(upload_to='videos_uploaded',null=True,
+        validators=[FileExtensionValidator(allowed_extensions=['MOV','avi','mp4','webm','mkv'])])
+    date_uploaded = models.DateTimeField(auto_now_add=True, null=True)
--- merger_tools/views.py
+++ merger_tools/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .form import *
 # Create your views here.
 
 def index(request):
-    return render(request, 'merger_tools/index.html')
+    video_form = VideoForm()
+    if request.method == 'POST':
+        video_form= VideoForm(request.POST, request.FILES)
+        #o_video = request.POST.get('original_video')
+        #m_video = request.POST.get('merged_video')
+        if video_form.is_valid():
+            video_form.save()
+            return redirect('templates')
+    context = {'video_fomr':video_form}
+    return render(request, 'merger_tools/index.html', context)
