2cdb527ad7553de5d994929ef9b0f0c8f507aa33
ararso <arasoalisho2@gmail.com>
2022-12-14 14:07:53 +0300

    update

--- merger_tools/urls.py
+++ merger_tools/urls.py
@@ -6,4 +6,6 @@ from django.urls import path
 urlpatterns = [
     
     path("", views.index, name = 'index'),
+    path('templates', views.templates, name='templates'),
+    path('setting', views.setting, name='setting'),
 ]
--- merger_tools/views.py
+++ merger_tools/views.py
@@ -6,10 +6,15 @@ def index(request):
     video_form = VideoForm()
     if request.method == 'POST':
         video_form= VideoForm(request.POST, request.FILES)
-        #o_video = request.POST.get('original_video')
-        #m_video = request.POST.get('merged_video')
         if video_form.is_valid():
             video_form.save()
             return redirect('templates')
     context = {'video_fomr':video_form}
     return render(request, 'merger_tools/index.html', context)
+
+def templates(request):
+    context = {}
+    return render(request, 'merger_tools/template.html', context)
+
+def setting(request):
+    return render(request, 'merger_tools/settings.html')
